20fd779a1b0e2024e4c712dcadf160f6c50f4e2f
Tom Tresansky <ttresansky@gradle.com>
2023-06-22 15:08:18 -0400

    Begin setting up ide-plugins project in new IDE platform

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -151,6 +151,11 @@ unassigned {
     subproject("instrumentation-declarations")
 }
 
+// IDE Platform
+platform("ide") {
+    subproject("ide-plugins")
+}
+
 // JVM Platform
 platform("jvm") {
     subproject("code-quality")
